76deb35dc6abf1abed92de039894e46d1e17a0e0
Wanda Antoine <wanda@thinkful.com>
2020-07-25 13:11:27 -0400

    added powerCalculator.js

---
+++ drills/powerCalculator.js
@@ -0,0 +1,25 @@
+// Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. 
+//The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers)
+
+// powerCalculator(10,2) should return 100
+// powerCalculator(10,-2) should return exponent should be >= 0
+
+function powerCalculator(n, pow){
+    // Base case
+if (pow < 0) {
+    console.log(`Pow must be >= 0`);
+} else if (pow === 0) {
+    // base case
+    return 1;
+} else {
+    // recursive case
+    // input to the recursive case is num and pow - 1
+    // output of the recursive all is the number multiplied by what is returned by powerCalculator
+    return n * powerCalculator(n, pow - 1);
+  }
+}
+
+// test the function
+console.log(powerCalculator(10, 2));
+
+module.exports = { powerCalculator };
